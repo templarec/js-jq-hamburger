4a7a9342178e83cc4d39f9205fad58a86ac3fbaf
Lorenzo Bernini <templarec@gmail.com>
2021-03-10 21:28:48 +0100

    Esercizio completato aggiunti commenti e durata animazione del menu a 2 secondi

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var menu = $(".fa-bars, .fa-times");
-var hamMenu = $(".hamburger-menu");
-menu.click( function (){
-  hamMenu.toggle();
+var menu = $(".fa-bars, .fa-times"); //dichiaro le classi alla var menu che mi servono per il click (icona burger e icona X)
+var hamMenu = $(".hamburger-menu"); // dichiaro classe del menu da visualizzare alla var
+menu.click( function (){ //evento click funzione anonima
+  hamMenu.toggle(2000); //toggle con una durata di 2 secondi, toggle attiva e disattiva il display block
 });
